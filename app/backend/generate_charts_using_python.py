f73b4012e281e8039ded56ba148ac6d47f352002
Alabouchsalaheddine <alabouchsalaheddine@gmail.com>
2025-01-28 18:41:48 +0100

    Chart generation refactor

--- app/backend/generate_charts_using_python.py
+++ app/backend/generate_charts_using_python.py
@@ -1,9 +1,22 @@
-from utils.chart_utils import results_to_dataframe
 import io
 import sys
 from contextlib import contextmanager
-import utils.llm_utils as llm_utils
 import re
+import pandas as pd
+import altair as alt
+from services.config_utils import load_config, get_config
+from dotenv import load_dotenv
+
+from services.llm_utils import make_llm_inference, instantiate_llm_model
+from services.config_utils import load_config, get_config
+
+config_path = "config.yaml"
+config = load_config(config_path)
+
+load_dotenv()
+
+selected_llm = get_config(config, "llms", "llm_name")
+
 @contextmanager
 def capture_print() :
     output_capture = io.StringIO()
@@ -19,7 +32,6 @@ def capture_print() :
 question = input("Please enter your question: ")
 
 
-selected_llm = "gpt4"
 
 invoke_parameters_dict = {"text" : question}
 language = "fr"
@@ -27,25 +39,20 @@ language = "fr"
 
 
 
-df = results_to_dataframe(query)
+df = pd.read_csv('static/data/data.csv')
 
 
-with capture_print() as captured_output :
-    if df is not None:
-        print(df.head())  # Display the first few rows of the DataFrame
-    else:
-        print("Failed to retrieve data.")
 
 
-pandas_dataframe =  captured_output.getvalue()
-print("pandas_dataframe : ", pandas_dataframe)
-
+pandas_dataframe = df.head(5).to_string()
 
 question_dict = {"question" : question}
 invoke_parameters_dict = {"question" : question, "pandas_dataframe" : pandas_dataframe}
 
-prompt_path = "./prompts/prompt_chart_generation_1.json"
-response = llm_utils.make_llm_inference(invoke_parameters_dict, prompt_path, selected_llm)
+prompt_path = "./prompts/prompt_chart_generation_code.json"
+model = instantiate_llm_model(selected_llm, temperature=0.2, max_tokens=2000)
+
+response = make_llm_inference(invoke_parameters_dict, prompt_path, model)
 print("response : ",response)
 llm_code = response.content
 
@@ -61,5 +68,13 @@ cleaned_code = re.search(r"```python(.*?)```", llm_code, re.DOTALL).group(1).str
 # Espace de noms pour passer la variable au code exécuté
 namespace = {"df": df}
 
-exec(cleaned_code, namespace)
+
+with capture_print() as captured_output :
+    exec(cleaned_code, namespace)
+
+
+code_execution_logs =  captured_output.getvalue()
+print("code_execution_logs : ", code_execution_logs)
+
+
 
